ef8e86cd1f4181cb43655cc15ac02dae56ffaaae
BoskoLukovic <65864865+BoskoLukovic@users.noreply.github.com>
2021-01-28 00:04:36 +0100

    Add files via upload
    todos

--- myJS.js
+++ myJS.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 
-    $(".card-header").click(function(){
-            $(".card-body").toggle();
+    $.get("https://jsonplaceholder.typicode.com/todos", function(data, status){
+        let tableString = "";
+        tableString +="<table class='table table-striped table-bordered w-100'><tr class='bg-dark text-light'><th class='py-2'>UserId</th><th>Id</th><th>Title</th><th>Completed</th></tr>";
+
+        data.forEach(element => {
+
+            if(element.completed == true){
+                tableString +="<tr style='color:green'><td>"+element.userId+"</td><td>"+element.id+"</td><td>"+element.title+"</td><td><button type='button' class='btn badge-success w-100'>Completed</button></td></tr>";   
+            }else{
+                tableString +="<tr style='color:red'><td>"+element.userId+"</td><td>"+element.id+"</td><td>"+element.title+"</td><td><button type='button' class='btn badge-danger w-100'>Completed</button></td></tr>";   
+            }
+     
+        });
+        tableString +="</table>";
+        $(".row").html(tableString);
     });
-$.get("https://jsonplaceholder.typicode.com/posts", function(data, status){
-
-    let tableString = "";
-    tableString += "<table class='table-striped table-bordered'><tr><th>Id</th><th>Title</th><th>Body</th></tr>";
-
-    data.forEach(element => {
-        tableString +="<tr><td>"+element.id+"</td><td>"+element.title+"</td><td>"+element.body+"</td></tr>";
-    });
-    
-    tableString +="</table>";
-    $(".row").html(tableString);
-
-});
 
 });
